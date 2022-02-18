fa1f9c9d53f6c2e3f72cbb4d6c2d06574b29bb32
isaacpitwa <isaacpitwa256@gmail.com>
2022-02-18 19:18:47 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by isaacpitwa on 18/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
